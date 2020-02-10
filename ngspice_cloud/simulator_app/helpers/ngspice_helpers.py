0aa1f5c76eef1c9ed5ee18d87293e8af823e9a00
Darsh Patel <darshkpatel@gmail.com>
2020-02-10 15:25:27 +0530

    Move Helpers

--- ngspice_cloud/simulator_app/helpers/ngspice_helpers.py
+++ ngspice_cloud/simulator_app/helpers/ngspice_helpers.py
@@ -12,7 +12,6 @@ def SetOutput(filepath):
     if os.path.isfile(filepath):
         proc = subprocess.Popen(['sed', '-i','-e', 's/run/set hcopydevtype=postscript\\nset hcopypscolor=1\\nset color0=white\\nset color1=black\\nset color2=red\\nset color3=blue\\nset color4=violet\\nset color5=rgb:3\/8\/0\\nset color6=rgb:4\/0\/0\\nset hcopywidth=800\\nset hcopyheight=600\\nrun\\nhardcopy v.ps allv\\nhardcopy all.ps all\\nhardcopy i.ps alli/', filepath])
         stdout,stderr = proc.communicate()
-        print(stdout)
         if bool(stderr) or proc.returncode==1:
             raise CannotAppendPlot("Sed command did not run successfully")
         else:
@@ -26,7 +25,16 @@ def ExecNetlist(filepath):
             SetOutput(filepath)
             proc = subprocess.Popen(['ngspice','-ab',filepath,'-o','output'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout,stderr = proc.communicate()
-            if bool(stderr) or proc.returncode==1:
+
+            if  bool(stderr) or (proc.returncode!=0 and proc.returncode!=1):
+
+                print(stderr)
+                print(proc.returncode)
+                print(stdout)
+                target = os.listdir('.')
+                for item in target:
+                    if (item.endswith(".ps")):
+                        os.remove(os.path.join('.', item))
                 raise CannotRunSpice("ngspice exited with error")
             else:
                 print('Ran ngSpice')
@@ -47,7 +55,3 @@ def ExecNetlist(filepath):
             
     else:
         raise IOError
-
-
-if __name__ == "__main__":
-    print(ExecNetlist("./RLC.cir.out"))
