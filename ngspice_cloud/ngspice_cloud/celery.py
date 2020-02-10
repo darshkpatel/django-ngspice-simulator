0f4da809e3dda7c0b21fbe8f4b3886c3b2ceb314
Darsh Patel <darshkpatel@gmail.com>
2020-02-10 15:02:31 +0530

    Celery Autodiscovery

--- ngspice_cloud/ngspice_cloud/celery.py
+++ ngspice_cloud/ngspice_cloud/celery.py
@@ -1,12 +1,14 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ngspice_cloud.settings')
 
 app = Celery('ngspice_cloud')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 @app.task(bind=True)
 def debug_task(self):
