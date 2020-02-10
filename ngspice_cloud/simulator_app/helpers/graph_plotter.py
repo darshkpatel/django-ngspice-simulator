a08cbf3fb4eb5953d0bceae3e239eb81c79575fa
Darsh Patel <darshkpatel@gmail.com>
2020-02-10 15:36:25 +0530

    Minor Fixes

--- ngspice_cloud/simulator_app/helpers/graph_plotter.py
+++ ngspice_cloud/simulator_app/helpers/graph_plotter.py
@@ -1,19 +1,22 @@
 from PIL import Image
 import os
 def LoadPSFile(path):
-    if os.path.isfile(filepath):
+    if os.path.isfile(path):
         im = Image.open(path)
         return im
     else:
         raise IOError
 
 def LoadPSFolder(path):
-    image_objects = []
-    if os.path.isdir(filepath):
+    if os.path.isdir(os.path.dirname(path)):
+        path = os.path.dirname(path)
+        print('Loading PS Files from ', path)
+        image_objects = []
         target = os.listdir(path)
         for item in target:
             if (item.endswith(".ps")):
                 image_objects.append(Image.open(os.path.join(path, item)))
         return image_objects
     else:
-        raise IOError
+        raise IOError('Path Not Directory')
+
