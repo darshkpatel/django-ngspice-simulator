c11de85aecd2711ebbedbf42f31eff7de73ba066
Darsh Patel <darshkpatel@gmail.com>
2020-02-15 17:28:04 +0530

    Verbose Logging

--- ngspice_cloud/simulator_app/helpers/graph_plotter.py
+++ ngspice_cloud/simulator_app/helpers/graph_plotter.py
@@ -1,11 +1,14 @@
 from PIL import Image
 import os
+
+
 def LoadPSFile(path):
     if os.path.isfile(path):
         im = Image.open(path)
         return im
     else:
         raise IOError
+
 
 def LoadPSFolder(file_id):
     path = '/tmp/ngspice-cloud-temp/'+file_id
@@ -16,11 +19,12 @@ def LoadPSFolder(file_id):
         for item in target:
             if (item.endswith(".ps")):
                 print('Plotting ', item)
-                im = Image.open(os.path.join(path, item))
+                ps_path = os.path.join(path, item)
+                im = Image.open(str(ps_path))
+                print("Opened Image ", ps_path)
                 file_path = os.path.join(path, item+'.png')
-                # im.save(file_path)
+                im.save(file_path)
                 image_objects.append(file_path)
         return image_objects
     else:
         raise IOError('Path Not Directory')
-
