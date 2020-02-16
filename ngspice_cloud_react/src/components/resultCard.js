b231fa25c98d69bb449b88c4e790a762d0056634
Darsh Patel <darshkpatel@gmail.com>
2020-02-16 15:49:05 +0530

    Auto show Scroll bar

--- ngspice_cloud_react/src/components/resultCard.js
+++ ngspice_cloud_react/src/components/resultCard.js
@@ -7,7 +7,6 @@ import {
 import './css/resultCard.css';
 import { Collapse, Container, Row, Button } from "shards-react";
 import SimpleStorage from "react-simple-storage";
-
 class ResultCard extends React.Component {
     constructor(props) {
         super(props);
@@ -56,8 +55,8 @@ class ResultCard extends React.Component {
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </CardHeader>
-                <CardBody className="cardBodyStyle">
-                    <Container>
+                <CardBody className="cardBodyStyle cardContainer">
+                    <Container className="">
                         <Row style={{ justifyContent: "space-between", flexDirection: 'row', alignItems: 'stretch' }}>
                             <h5>Status:</h5><strong>{status}</strong> {'\u00A0'}{'\u00A0'}
                             {this.state.pollResult.state === 'SUCCESS' &&
@@ -67,9 +66,9 @@ class ResultCard extends React.Component {
                         <Row>
                             <Collapse open={this.state.collapse}>
                                 {this.state.pollResult.details[0] != null &&
-                                    <div>
+                                    <div><hr/>
                                         <h5>Output</h5>
-                                        <p>{JSON.stringify(this.state.pollResult.details[0])}</p>
+                                        <p>{this.state.pollResult.details[0]}</p>
                                     </div>
                                 }
                                 {typeof this.state.pollResult.details[1] != "undefined"  
