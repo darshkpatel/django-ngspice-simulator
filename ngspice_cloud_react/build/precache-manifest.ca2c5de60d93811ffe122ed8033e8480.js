3e960b39139964b9da1da1a310bcfab198294a6f
Darsh Patel <darshkpatel@gmail.com>
2020-02-16 15:51:03 +0530

    Add Build Files

---
+++ ngspice_cloud_react/build/precache-manifest.ca2c5de60d93811ffe122ed8033e8480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1abc94eeefc32ca30eef2c153629d82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6494d97b06cae37e9547",
+    "url": "/static/css/2.6ea3de80.chunk.css"
+  },
+  {
+    "revision": "05172494356ed344a05d",
+    "url": "/static/css/main.10857f5f.chunk.css"
+  },
+  {
+    "revision": "6494d97b06cae37e9547",
+    "url": "/static/js/2.69e29dae.chunk.js"
+  },
+  {
+    "revision": "f50c51fc29f55d8a26911b32578db2d5",
+    "url": "/static/js/2.69e29dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05172494356ed344a05d",
+    "url": "/static/js/main.b6880a95.chunk.js"
+  },
+  {
+    "revision": "80a00796b7d7dfb96d86",
+    "url": "/static/js/runtime-main.921e2665.js"
+  }
+]);
