ad902a7638415ab7eaf3fb88a8546554bf3afdd0
Darsh Patel <darshkpatel@gmail.com>
2020-02-16 16:46:01 +0530

    Fix App Test

--- ngspice_cloud_react/src/App.test.js
+++ ngspice_cloud_react/src/App.test.js
@@ -3,6 +3,5 @@ import { render } from '@testing-library/react';
 import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<App />, div);
 });
