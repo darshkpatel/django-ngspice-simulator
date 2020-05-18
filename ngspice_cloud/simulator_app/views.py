24ee2d6bb1bdf20d2effa32211f3e295203b79bc
Darsh <darshkpatel@gmail.com>
2020-05-18 21:40:54 +0530

    Remove stray imports

--- ngspice_cloud/simulator_app/views.py
+++ ngspice_cloud/simulator_app/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
-import json
 import uuid
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny
 from celery.result import AsyncResult
 from upload_app.models import Task
-from django.shortcuts import get_list_or_404, get_object_or_404
-from rest_framework import status
+from django.shortcuts import get_object_or_404
 from upload_app.serializers import TaskSerializer
 from rest_framework.exceptions import ValidationError
 from simulator_app.tasks import process_task
@@ -58,7 +55,6 @@ class TaskStartView(APIView):
         if isinstance(task_id, uuid.UUID):
             group_task = process_task.apply_async(
                 kwargs={'task_id': str(task_id)}, task_id=str(task_id))
-            celery_result = AsyncResult(str(task_id))
             task = get_object_or_404(Task, task_id=task_id)
             serializer = TaskSerializer(task, many=False)
             response_data = {
