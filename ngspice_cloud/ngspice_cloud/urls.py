1ff00ec8c41f3bd88118d11489427c6b7a611cd3
Darsh <darshkpatel@gmail.com>
2020-05-18 21:32:40 +0530

    Linting Fixes

--- ngspice_cloud/ngspice_cloud/urls.py
+++ ngspice_cloud/ngspice_cloud/urls.py
@@ -14,14 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
-from user_auth_app import views as auth_views
 from upload_app import views as upload_views
 from simulator_app import views as simulator_views
 from rest_framework import routers
-from django.conf import settings
-from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'uploads', upload_views.UploadViewSet)
@@ -29,10 +26,16 @@ router.register(r'uploads', upload_views.UploadViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/task/<uuid:task_id>', simulator_views.TaskResultView.as_view(), name='task_status'),
+    path('api/task/<uuid:task_id>',
+         simulator_views.TaskResultView.as_view(), name='task_status'),
+
     path('api/task', simulator_views.ViewTasks.as_view(), name='view_tasks'),
-    path('api/task/<uuid:task_id>/start', simulator_views.TaskStartView.as_view(), name='task_start'),
-    path('api/celery/<uuid:task_id>', simulator_views.CeleryResultView.as_view(), name='celery_status'),
+
+    path('api/task/<uuid:task_id>/start',
+         simulator_views.TaskStartView.as_view(), name='task_start'),
+
+    path('api/celery/<uuid:task_id>',
+         simulator_views.CeleryResultView.as_view(), name='celery_status'),
 
     path('api/', include(router.urls)),
 
